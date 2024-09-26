b412646ffa93e4cfad06de1ce76e572a073aee0e
Mohadevsn <mohawade23@gmail.com>
2024-09-26 12:44:51 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <img src=" " alt="profile image"/>
+            <h2>Mohamed</h2><h3>Wade</h3>
+        </div>
+    )
+}
